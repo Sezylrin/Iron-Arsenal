8c2aa5b66345cd0639e4c25f5bd83dda6c0f5fa8
Xero8252 <39457241+Xero-oce@users.noreply.github.com>
2023-04-01 15:28:12 +1100

    Gun Following Mouse

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    private Transform rotatePoint;
+    private Vector3 mouseLocation;
+    private Vector3 worldPosition;
+
+    void Awake()
+    {
+        rotatePoint = GameObject.Find("Gun Rotation Point").transform;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseLocation = Input.mousePosition;
+        mouseLocation.z += 90;
+
+        worldPosition = Camera.main.ScreenToWorldPoint(mouseLocation);
+        worldPosition.y = 0;
+
+        rotatePoint.LookAt(worldPosition);
     }
 }
