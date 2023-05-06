37b4212c7c24be24223e07308f68c879c79c6c15
Allan Thang <allan.thang1920@gmail.com>
2023-05-07 03:46:57 +1000

    US35 - Events spawn further apart and in larger radius
    Events aren't coded to despawn when far away from player yet. Might change this if radar system allows.
    Spawn events further apart by changing 'Min Tiles Btwn Events'
    Change radius of spawning by changing 'Event Radius'
    Tick the 'Enemy Manager In Scene' bool when putting pcg in scene to limit spawning based on difficulty (controlled by 'DC To Start Spawn')

--- Assets/Scripts/PCG/MapGenerator.cs
+++ Assets/Scripts/PCG/MapGenerator.cs
@@ -19,7 +19,7 @@ public class MapGenerator : MonoBehaviour
     {
         public GameObject objectToSpawn;
         public float chanceToSpawn;
-        public int waveToStartSpawning;
+        public int DCToStartSpawning;
     }
     public List<ObjectTiles> objectTilesList = new List<ObjectTiles>();
 
@@ -30,14 +30,16 @@ public class MapGenerator : MonoBehaviour
     public Transform groundTilesContainer;
     public Transform objectTilesContainer;
 
-    // [Range(0, 50)] public float chanceOfNovacite;
-    // [Range(0, 50)] public float chanceOfVoidstone;
-    // [Range(0, 50)] public float chanceOfXenorium;
-
-    [Header("Radius is how many tiles should spawn around the player")]
-    public int radius = 10;
-    [Header("Tile Offset is how big the tiles are so they don't overlap")]
-    public int tileOffset = 1;
+    [Header("How far away from the player that ground will spawn")]
+    public int groundRadius = 10;
+    [Header("How far away from the player that events will spawn")]
+    public int eventRadius = 20;
+    [Header("How big the tiles are so they don't overlap")]
+    public int tileOffset = 9;
+    [Header("Tiles between event spawns")]
+    public int minTilesBtwnEvents;
+    [Header("Turn this on in scene with an enemy manager to activate DCToStartSpawning")]
+    public bool EnemyManagerInScene = false;
 
     private Vector3 startPos = Vector3.zero;
 
@@ -47,14 +49,17 @@ public class MapGenerator : MonoBehaviour
     private int XPlayerLocation => (int)Mathf.Floor(player.transform.position.x / tileOffset) * tileOffset;
     private int ZPlayerLocation => (int)Mathf.Floor(player.transform.position.z / tileOffset) * tileOffset;
 
-    private Dictionary<Vector3, Tile> allGroundTiles = new Dictionary<Vector3, Tile>();
-    private Dictionary<Vector3, Tile> activeGroundTiles = new Dictionary<Vector3, Tile>();
-    private Dictionary<Vector3, Tile> allObjectTiles = new Dictionary<Vector3, Tile>();
-    private Dictionary<Vector3, Tile> activeObjectTiles = new Dictionary<Vector3, Tile>();
+    private Dictionary<Vector3, GroundTile> allGroundTiles = new Dictionary<Vector3, GroundTile>();
+    private Dictionary<Vector3, GroundTile> activeGroundTiles = new Dictionary<Vector3, GroundTile>();
+    private Dictionary<Vector3, EventTile> allEventTiles = new Dictionary<Vector3, EventTile>();
+    private Dictionary<Vector3, EventTile> activeEventTiles = new Dictionary<Vector3, EventTile>();
 
     private void Start()
     {
         GenerateTiles();
+        // GameObject tileInstance = Instantiate(emptyTile, new Vector3(0.0f,0.01f,0.0f), Quaternion.identity, objectTilesContainer);
+        // EventTile tile = new EventTile(tileInstance, true);
+        // allEventTiles[Vector3.zero] = tile;
     }
 
     private void Update()
@@ -65,27 +70,27 @@ public class MapGenerator : MonoBehaviour
 
     private void GenerateTiles()
     {
-        var newActiveGroundTiles = new Dictionary<Vector3, Tile>();
-        var newActiveObjectTiles = new Dictionary<Vector3, Tile>();
+        var newActiveGroundTiles = new Dictionary<Vector3, GroundTile>();
+        var newActiveObjectTiles = new Dictionary<Vector3, GroundTile>();
         float cTime = Time.realtimeSinceStartup;
 
-        for (int x = -radius; x <= radius; x++)
+        for (int x = -groundRadius; x <= groundRadius; x++)
         {
-            for (int z = -radius; z <= radius; z++)
+            for (int z = -groundRadius; z <= groundRadius; z++)
             {
                 Vector3 pos = new Vector3(x * tileOffset + XPlayerLocation, 0, z * tileOffset + ZPlayerLocation);
 
                 // If a ground tile hasn't spawned in that position yet
                 if (!allGroundTiles.ContainsKey(pos))
                 {
-                    SpawnNewTile(pos, cTime);
+                    SpawnNewGroundTile(pos, cTime);
                 }
                 // If a tile occupies that position
                 else
                 {
                     // Update the timestamp to be the current time
                     allGroundTiles[pos].cTimestamp = cTime;
-                    allObjectTiles[pos].cTimestamp = cTime;
+                    // allObjectTiles[pos].cTimestamp = cTime;
                     // If the tile is not active
                     if (!activeGroundTiles.ContainsKey(pos))
                     {
@@ -93,14 +98,27 @@ public class MapGenerator : MonoBehaviour
                         allGroundTiles[pos].tileObject.SetActive(true);
                         activeGroundTiles.Add(pos, allGroundTiles[pos]);
                     }
-                    if (!activeObjectTiles.ContainsKey(pos))
-                    {
-                        if (allObjectTiles[pos].tileObject && allObjectTiles[pos].tileObject != emptyTile)
-                        {
-                            allObjectTiles[pos].tileObject.SetActive(true);
-                            activeObjectTiles.Add(pos, allObjectTiles[pos]);
-                        }
-                    }
+                    // if (!activeObjectTiles.ContainsKey(pos))
+                    // {
+                    //     if (allObjectTiles[pos].tileObject && allObjectTiles[pos].tileObject != emptyTile)
+                    //     {
+                    //         allObjectTiles[pos].tileObject.SetActive(true);
+                    //         activeObjectTiles.Add(pos, allObjectTiles[pos]);
+                    //     }
+                    // }
+                }
+            }
+        }
+
+        for (int x = -eventRadius; x <= eventRadius; x++)
+        {
+            for (int z = -eventRadius; z <= eventRadius; z++)
+            {
+                Vector3 pos = new Vector3(x * tileOffset + XPlayerLocation, 0, z * tileOffset + ZPlayerLocation);
+
+                if (!allEventTiles.ContainsKey(pos))
+                {
+                    SpawnNewObjectTile(pos, cTime);
                 }
             }
         }
@@ -120,25 +138,25 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
-        foreach (var tile in activeObjectTiles)
-        {
-            // If they aren't in bounds of current time
-            if (!tile.Value.cTimestamp.Equals(cTime))
-            {
-                if (tile.Value.tileObject && tile.Value.tileObject != emptyTile)
-                {
-                    // Disable object
-                    tile.Value.tileObject.SetActive(false);
-                }
-            }
-            else
-            {
-                newActiveObjectTiles.Add(tile.Key, tile.Value);
-            }
-        }
+        // foreach (var tile in activeEventTiles)
+        // {
+        //     // If they aren't in bounds of current time
+        //     if (!tile.Value.cTimestamp.Equals(cTime))
+        //     {
+        //         if (tile.Value.tileObject && tile.Value.tileObject != emptyTile)
+        //         {
+        //             // Disable object
+        //             tile.Value.tileObject.SetActive(false);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         newActiveObjectTiles.Add(tile.Key, tile.Value);
+        //     }
+        // }
 
         activeGroundTiles = newActiveGroundTiles;
-        activeObjectTiles = newActiveObjectTiles;
+        // activeEventTiles = newActiveObjectTiles;
         startPos = player.transform.position;
     }
 
@@ -152,7 +170,7 @@ public class MapGenerator : MonoBehaviour
     private void SpawnNewGroundTile(Vector3 pos, float cTime)
     {
         GameObject tileInstance = Instantiate(groundTile, pos, Quaternion.identity, groundTilesContainer);
-        Tile tile = new Tile(cTime, tileInstance);
+        GroundTile tile = new GroundTile(cTime, tileInstance);
         activeGroundTiles.Add(pos, tile);
         allGroundTiles.Add(pos, tile);
     }
@@ -161,37 +179,20 @@ public class MapGenerator : MonoBehaviour
     {
         GameObject tileType = RandomTile();
         GameObject tileInstance;
-        if (!tileType)
-        {
-            tileInstance = emptyTile;
-        }
-        else
+        EventTile tile = new EventTile();
+        if (tileType && !EventIsNearOtherEvents(pos))
         {
             tileInstance = Instantiate(tileType, pos + new Vector3(0.0f,0.01f,0.0f), Quaternion.identity, objectTilesContainer);
+            tile = new EventTile(tileInstance);
         }
-        Tile tile = new Tile(cTime, tileInstance);
-        activeObjectTiles.Add(pos, tile);
-        allObjectTiles.Add(pos, tile);
+        // activeEventTiles.Add(pos, tile);
+        allEventTiles.Add(pos, tile);
     }
 
     private GameObject RandomTile()
     {
         int random = Random.Range(0, 1000);
         GameObject tileType = null;
-
-        // Old code just in case
-        // if (0 < random && random <= chanceOfNovacite)
-        // {
-        //     tileType = objectTiles.novacite;
-        // }
-        // if (chanceOfNovacite < random && random <= chanceOfVoidstone + chanceOfNovacite)
-        // {
-        //     tileType = objectTiles.voidstone;
-        // }
-        // if (chanceOfVoidstone + chanceOfNovacite < random && random <= chanceOfNovacite + chanceOfVoidstone + chanceOfXenorium)
-        // {
-        //     tileType = objectTiles.xenorium;
-        // }
 
         for (int i = 0; i < objectTilesList.Count; i++)
         {
@@ -202,7 +203,7 @@ public class MapGenerator : MonoBehaviour
                 lowerBound += objectTilesList[j].chanceToSpawn;
             }
             upperBound = objectTilesList[i].chanceToSpawn + lowerBound;
-            if (lowerBound <= random && random < upperBound && EnemyManager.Instance.Difficulty >= objectTilesList[i].waveToStartSpawning)
+            if (lowerBound <= random && random < upperBound || EnemyManagerInScene && EnemyManager.Instance.Difficulty >= objectTilesList[i].DCToStartSpawning)
             {
                 tileType = objectTilesList[i].objectToSpawn;
             }
@@ -211,17 +212,66 @@ public class MapGenerator : MonoBehaviour
         return tileType;
     }
 
-    private class Tile
+    private class GroundTile
     {
         public float cTimestamp;
         public GameObject tileObject;
 
-        public Tile(float cTimestamp, GameObject tileObject)
+        public GroundTile(float cTimestamp, GameObject tileObject)
         {
             this.cTimestamp = cTimestamp;
             this.tileObject = tileObject;
         }
     }
 
+    private class EventTile
+    {
+        public bool isEmpty;
+        public GameObject tileObject;
+
+        public EventTile()
+        {
+            isEmpty = true;
+        }
+
+        public EventTile(GameObject tileObject)
+        {
+            isEmpty = false;
+            this.tileObject = tileObject;
+        }
+    }
+
     private bool PlayerHasMoved() => Mathf.Abs(XPlayerMove) >= tileOffset || Mathf.Abs(ZPlayerMove) >= tileOffset;
+
+    private bool EventIsNearOtherEvents (Vector3 pos)
+    {
+        for (int i = 1; i <= minTilesBtwnEvents; i++)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int z = -1; z <= 1; z++)
+                {
+                    if (x == 0 && z == 0) continue;
+                    if (Mathf.Abs(x) != Mathf.Abs(z))
+                    {
+                        Vector3 posToCheck = pos + new Vector3(tileOffset * x, 0, tileOffset * z) * i;
+                        if (allEventTiles.ContainsKey(posToCheck) && !allEventTiles[posToCheck].isEmpty)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        Vector3 posToCheck = pos + new Vector3(tileOffset * x, 0, tileOffset * z) * (i-1);
+                        if (allEventTiles.ContainsKey(posToCheck) && !allEventTiles[posToCheck].isEmpty)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
