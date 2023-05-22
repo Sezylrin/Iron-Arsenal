a1ee0fedd81ca35f927ae5a9b721fbb22a6b5087
Allan Thang <allan.thang1920@gmail.com>
2023-05-22 22:32:24 +1000

    US34 - Added Cooldown and image fill to radar button

--- Assets/Scripts/Radar/Radar.cs
+++ Assets/Scripts/Radar/Radar.cs
@@ -82,13 +82,9 @@ public class Radar : MonoBehaviour
         }
 
         button.onClick.AddListener(() => RevealEvent(Scan(spawnableEventsList[i])));
-        button.onClick.AddListener(ToggleRadarMenu);
+        button.onClick.AddListener(RadarCanvasManager.Instance.ToggleRadarMenu);
+        button.onClick.AddListener(RadarCanvasManager.Instance.ResetRadarCooldown);
         buttonsList.Add(button);
-    }
-
-    public void ToggleRadarMenu()
-    {
-        gameObject.SetActive(!gameObject.activeSelf);
     }
 
     private MapGenerator.EventTile Scan(MapGenerator.SpawnableEvent eventToSearchFor)
@@ -129,4 +125,63 @@ public class Radar : MonoBehaviour
             button.onClick.RemoveAllListeners();
         }
     }
+
+    // public class RadarCanvasManager : MonoBehaviour
+    // {
+    //     public static RadarCanvasManager Instance { get; private set; }
+    //     public Image radarButton;
+    //     public float cooldown = 60;
+    //     public GameObject radarMenu;
+    //
+    //     private float radarTimer = 0;
+    //     public bool isRadarOffCooldown = true;
+    //
+    //     private void Awake()
+    //     {
+    //         if (Instance != null)
+    //         {
+    //             DestroyImmediate(Instance);
+    //         }
+    //         else
+    //         {
+    //             Instance = this;
+    //         }
+    //     }
+    //
+    //     void Start()
+    //     {
+    //         isRadarOffCooldown = true;
+    //     }
+    //
+    //     void Update()
+    //     {
+    //         TickRadarCooldown();
+    //     }
+    //
+    //     private void TickRadarCooldown()
+    //     {
+    //         radarTimer += Time.deltaTime;
+    //         if (radarTimer >= cooldown) isRadarOffCooldown = true;
+    //         if (!isRadarOffCooldown)
+    //         {
+    //             radarButton.fillAmount = radarTimer/cooldown;
+    //         }
+    //         else
+    //         {
+    //             radarButton.fillAmount = 1;
+    //         }
+    //     }
+    //
+    //     public void ResetRadarCooldown()
+    //     {
+    //         radarTimer = 0;
+    //         isRadarOffCooldown = false;
+    //     }
+    //
+    //     public void ToggleRadarMenu()
+    //     {
+    //         if (!isRadarOffCooldown) return;
+    //         radarMenu.SetActive(!radarMenu.activeSelf);
+    //     }
+    // }
 }
